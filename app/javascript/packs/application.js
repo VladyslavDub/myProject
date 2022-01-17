ba09a16e06044dd17eff793762f24caa1a2968c7
VladyslavDub <vladik.dub.2003@gmail.com>
2022-01-17 16:05:23 +0200

    blog

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "../stylesheets/application"
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,6 @@
 class Pin < ApplicationRecord
     has_one_attached :image
+
+    validates :title, presence: true
+    validates :caption, presence: true, length: { minimum: 5 }
 end
